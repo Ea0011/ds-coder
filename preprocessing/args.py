cadea5e9bf289b512b605e0ee8e1b0c4a5c6bcb4
Ea0011 <avagyanee@gmail.com>
2023-12-07 19:59:55 +0400

    feat: add code linem length and comment filters

--- preprocessing/args.py
+++ preprocessing/args.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Optional
 
 
 @dataclass
@@ -8,16 +8,51 @@ class FilteringArgs:
         metadata={"help": "Name of the parquet file in the raw folder"}
     )
 
-    regex_filters_path: str = field(
+    regex_filters_path: Optional[str] = field(
         default="./preprocessing/regex_filters.json",
         metadata={"help": "Path to a json file with regex patterns for filtering"},
     )
 
-    save_path: str = field(
-        default="./data/filtered/",
-        metadata={"help": "Where to save the filtered dataset file"},
+    save_name: Optional[str] = field(
+        default="filtered",
+        metadata={"help": "Name of the saved file in filtered folder"},
     )
 
-    log_file: str = field(
+    log_file: Optional[str] = field(
         default="./filtering.log", metadata={"help": "Path to the log file"}
+    )
+
+    line_max: Optional[int] = field(
+        default=1000,
+        metadata={"help": "Max line length allowed"},
+    )
+
+    line_mean: Optional[int] = field(
+        default=100,
+        metadata={"help": "Mean line length allowed"},
+    )
+
+    alpha_frac: Optional[float] = field(
+        default=0.25,
+        metadata={"help": "Minimum fraction of alphanumeric characters allowed."},
+    )
+
+    min_size: Optional[int] = field(
+        default=100,
+        metadata={"help": "Minimum content size."},
+    )
+
+    max_size: Optional[int] = field(
+        default=5000,
+        metadata={"help": "Maximum content size."},
+    )
+
+    max_threshold_comments: Optional[float] = field(
+        default=0.8,
+        metadata={"help": "Maximum threshold for comment to code ratio."},
+    )
+
+    output_col_name: Optional[str] = field(
+        default="output",
+        metadata={"help": "The name of the column to predict with the model"},
     )
--- preprocessing/filtering.py
+++ preprocessing/filtering.py
@@ -4,30 +4,70 @@ from preprocessing.args import FilteringArgs
 from simple_parsing import parse
 import json
 import pandas as pd
+from functools import reduce
+from preprocessing.utils import get_nl_ratio
+import re
 
 
-def parse_args():
+def parse_args() -> FilteringArgs:
     return parse(FilteringArgs)
 
 
-def get_regex_patterns(patterns_path: str) -> list[str]:
-    with open(file=patterns_path) as f:
+def get_regex_patterns() -> list[str]:
+    with open(file=args.regex_filters_path) as f:
         filters = json.load(f)
 
     return filters
 
 
 def filter_regex_patterns(df: pd.DataFrame, regex_filters: list[str]) -> pd.DataFrame:
-    column, regex = list(regex_filters.keys())[0], "|".join(
-        list(regex_filters.values())[0]
+    filtered_idx = []
+    for col_name, regex in regex_filters.items():
+        filtered = df[col_name].str.contains("|".join(regex), regex=True)
+        filtered_idx.append(filtered)
+
+    union_of_filters = reduce(lambda a, b: a | b, filtered_idx)
+
+    return df[union_of_filters]
+
+
+def filter_long_and_short_answers(df: pd.DataFrame) -> pd.DataFrame:
+    len_filter_idx = df[args.output_col_name].apply(
+        lambda s: len(s) <= args.max_size and len(s) >= args.min_size
     )
-    filtered = df[column].str.contains(regex, regex=True)
-
-    return df[filtered]
+    return df[len_filter_idx]
 
 
-def load_dataset_df(dataset_name: str) -> pd.DataFrame:
-    df = pd.read_parquet(f"./data/raw/{dataset_name}.parquet")
+def filter_line_length(df: pd.DataFrame) -> pd.DataFrame:
+    max_len_filter = lambda s: len(max(s.split("\n"), key=len)) <= args.line_max
+    mean_len_filter = (
+        lambda s: sum(map(len, s.split("\n"))) / len(s.split("\n")) <= args.line_mean
+    )
+
+    line_len_filter_max_idx = df[args.output_col_name].apply(max_len_filter)
+    line_len_filter_mean_idx = df[args.output_col_name].apply(mean_len_filter)
+
+    return df[line_len_filter_mean_idx & line_len_filter_max_idx]
+
+
+def filter_alphanum_ratio(df: pd.DataFrame) -> pd.DataFrame:
+    alphanum_ratio_filter = (
+        lambda s: sum(map(len, re.findall("\w+", s))) / len(s) >= args.alpha_frac
+    )
+
+    return df[df[args.output_col_name].apply(alphanum_ratio_filter)]
+
+
+def filter_comment_ratio(df: pd.DataFrame) -> pd.DataFrame:
+    comment_ratio_filter = (
+        lambda s: get_nl_ratio(s, "python") < args.max_threshold_comments
+    )
+
+    return df[df[args.output_col_name].apply(comment_ratio_filter)]
+
+
+def load_dataset_df() -> pd.DataFrame:
+    df = pd.read_parquet(f"./data/raw/{args.dataset_name}.parquet")
 
     return df
 
@@ -47,14 +87,42 @@ if __name__ == "__main__":
         f"** The job is running with the following arguments: **\n{args}\n **** "
     )
 
-    filters = get_regex_patterns(args.regex_filters_path)
+    filters = get_regex_patterns()
 
     logger.info(f"** Loaded filters are: {filters}")
 
-    df = load_dataset_df(args.dataset_name)
+    df = load_dataset_df()
     regex_filtered = filter_regex_patterns(df, filters)
 
     logger.info(
         f"** Regex filtered with filters {filters}\n \
-      Remaining rows: {regex_filtered.count(axis=0)}"
+        Remaining rows: {regex_filtered.count(axis=0)}"
+    )
+
+    length_filtered = filter_long_and_short_answers(regex_filtered)
+
+    logger.info(
+        f"** Filtered with min len {args.min_size} and max len {args.max_size} \n \
+        Remaining rows: {length_filtered.count(axis=0)}"
+    )
+
+    line_len_filtered = filter_line_length(length_filtered)
+
+    logger.info(
+        f"** Filtered line len with mean {args.line_mean} and max len {args.line_max} \n \
+        Remaining rows: {line_len_filtered.count(axis=0)}"
+    )
+
+    alphanum_filtered = filter_alphanum_ratio(line_len_filtered)
+
+    logger.info(
+        f"** Filtered alphanumeric ratio with min ratio of {args.alpha_frac} \n \
+        Remaining rows: {alphanum_filtered.count(axis=0)}"
+    )
+
+    comment_ratio_filtered = filter_alphanum_ratio(alphanum_filtered)
+
+    logger.info(
+        f"** Filtered comment to code ratio with max ratio of {args.alpha_frac} \n \
+        Remaining rows: {comment_ratio_filtered.count(axis=0)}"
     )
